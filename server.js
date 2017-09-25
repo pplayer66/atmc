2f271f98627a2e5997dd9d318b9438e263394ae5
player89 <player89@yahoo.com>
2017-09-25 07:43:51 +0600

    .

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// const favicon = require('serve-favicon');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// app.use(favicon(path.join(__dirname, 'favicon', 'favicon.ico')));
+app.set('view engine', 'pug');
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+	res.render('index');
+});
+
+app.get('/:page?', (req,res)=>{
+	if (req.params.page){
+		res.redirect('/');
+	}
+});
+
+app.listen(port, ()=>{
+	console.log('Server is running...');
+});
