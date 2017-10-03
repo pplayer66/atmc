fbfe32fecbc2af48cc821da165957bde4924232d
player89 <player89@yahoo.com>
2017-10-03 21:21:40 +0600

    .

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,15 +1,15 @@
 var i = 0;
 var timer;
 var $active = $('.active');
-$active.parent().addClass('col7-hover');
+$active.parent().addClass('product-box-active');
 
-$('.section-col-7').hover(function() {
+$('.product-box').hover(function() {
 	clearInterval(timer);
 	$active.fadeOut(300);
-	$active.parent().removeClass('col7-hover');
+	$active.parent().removeClass('product-box-active');
 	$active = $(this).find('.product-container')
 	$active.fadeIn(300);
-	$active.parent().addClass('col7-hover');
+	$active.parent().addClass('product-box-active');
 	i = $(this).index()+1-1;
 },function() {
 	clearInterval(timer);
@@ -21,10 +21,10 @@ var startSlider = function() {
 		i=0;
 	i++;
 	$active.fadeOut(300);
-	$active.parent().removeClass('col7-hover');
+	$active.parent().removeClass('product-box-active');
 	$active = $('.product-container').eq(i%7);
 	$active.fadeIn(300);
-	$active.parent().addClass('col7-hover');
+	$active.parent().addClass('product-box-active');
 };
 
 var resumeTimer = function() {
