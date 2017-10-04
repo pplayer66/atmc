d5e7404449f2c7cc5433d943b55a835c6cdd801e
player89 <player89@yahoo.com>
2017-10-04 22:15:19 +0600

    .

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 // const favicon = require('serve-favicon');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 
 // app.use(favicon(path.join(__dirname, 'favicon', 'favicon.ico')));
 app.set('view engine', 'pug');
@@ -12,6 +12,10 @@ app.use(express.static('public'))
 
 app.get('/', (req, res)=>{
 	res.render('index');
+});
+
+app.get('/price', (req, res)=>{
+	res.sendFile(`${__dirname}/public/static/Каталог ТОО АТМ.ppt`);
 });
 
 app.get('/:page?', (req,res)=>{
